5559cd065ba45c825a5b01659e26b0a7b80fecff
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-24 20:18:37 -0300

    Estrutura inicial da calculadora de IMC

---
+++ CalculaIMC/Program.cs
@@ -0,0 +1,25 @@
+﻿#region Calculadora de IMC
+
+string Nome;
+double Peso;
+double Altura;
+double Imc;
+
+Console.WriteLine("Digite seu nome: ");
+Nome = Console.ReadLine();
+Console.WriteLine("Seu nome digitado foi: " + Nome);
+
+Console.WriteLine();
+
+Console.WriteLine("Digite seu peso (kg): ");
+Peso = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("O peso digitado foi: " + Peso);
+
+Console.WriteLine();
+
+Console.WriteLine("Digite sua altura (m): ");
+Altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Sua altura digitada foi: " + Altura);
+
+
+#endregion
