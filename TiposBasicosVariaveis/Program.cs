8545990a752221f0e8331d827b86c3be72300579
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-24 19:41:40 -0300

    aplicação console para testar os tipos primitivos de váriaveis

---
+++ TiposBasicosVariaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿#region tipos basicos de variaveis com c#
+
+//tipos primitivos
+int numero = 10;
+double preco = 19.99;
+char letra = 'A';
+string nome = "Aline";
+bool ativo = true;
+
+
+
+Console.WriteLine("Número: " + numero);
+Console.WriteLine("preço: " + preco);
+Console.WriteLine("letra: " + letra);
+Console.WriteLine("Nome: " + nome);
+Console.WriteLine("Ativo: " + ativo);
+#endregion
