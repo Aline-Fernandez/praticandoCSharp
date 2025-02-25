379a4f2cbafc3a3bd5a8951907ace6804f088c2c
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-24 23:16:29 -0300

    adicionado validações para verificar resultados do IMC

--- CalculaIMC/Program.cs
+++ CalculaIMC/Program.cs
@@ -1,25 +1,64 @@
 ﻿#region Calculadora de IMC
 
+using System.ComponentModel.Design;
+
 string Nome;
 double Peso;
 double Altura;
 double Imc;
 
-Console.WriteLine("Digite seu nome: ");
+Console.WriteLine("Preencha os campos para o calculo do seu IMC");
+
+Console.WriteLine("\nNome: ");
 Nome = Console.ReadLine();
 Console.WriteLine("Seu nome digitado foi: " + Nome);
 
 Console.WriteLine();
 
-Console.WriteLine("Digite seu peso (kg): ");
+Console.WriteLine("Peso (kg): ");
 Peso = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("O peso digitado foi: " + Peso);
 
 Console.WriteLine();
 
-Console.WriteLine("Digite sua altura (m): ");
+Console.WriteLine("Altura (m): ");
 Altura = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Sua altura digitada foi: " + Altura);
+Console.WriteLine("Sua altura digitada foi: " + Altura.ToString("F2"));
+
+Imc = Peso / (Altura * Altura);
+Console.WriteLine("\nO seu IMC é: " + Imc.ToString("F2"));
+
+Console.WriteLine("\n______________________________________________________________________");
+
+  if (Imc < 18.5)
+{
+    Console.WriteLine(Nome + "\nSeu IMC é: " + Imc.ToString("F2") + "\nAbaixo do peso");
+}
+
+  else if (Imc >= 18.5 && Imc <= 24.9)
+{
+    Console.WriteLine(Nome + "\nSeu IMC é: " + Imc.ToString("F2") + "\nPeso normal");
+}
+
+ else if (Imc >= 25 && Imc <= 29.9)
+{
+    Console.WriteLine(Nome + "\nSeu IMC é: " + Imc.ToString("F2") + "\nSobrePeso");
+}
+ else if (Imc >= 30 && Imc <= 34.9)
+        {
+            Console.WriteLine(Nome + "\nSeu IMC é: " + Imc.ToString("F2") + "\nObesidade Grau 1");
+        }
+
+ else if (Imc >= 35 && Imc <= 39.9)
+{
+    Console.WriteLine(Nome + "\nSeu IMC é: " + Imc.ToString("F2") + "\nObesidade Grau 2");
+}
+
+else
+        {
+    Console.WriteLine(Nome + "\nSeu IMM é: " + Imc.ToString("F2") + "\nObesidade Grau 3");
+}
+
 
 
 #endregion
