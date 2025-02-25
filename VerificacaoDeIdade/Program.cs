ae6d151e4e368e0b3bcc07c2a00d2c1710a4ea61
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-25 13:11:27 -0300

    Veririficação de idade do usuário

---
+++ VerificacaoDeIdade/Program.cs
@@ -0,0 +1,34 @@
+﻿#region codigo para verificar a idade do usuario
+
+string Nome;
+int Idade;
+
+Console.WriteLine("Digite seu Nome: ");
+Nome = Console.ReadLine();
+
+Console.WriteLine();
+
+Console.WriteLine("Digite sua idade: ");
+Idade = int.Parse(Console.ReadLine());
+
+if (Idade < 13)
+{
+    Console.WriteLine("Você é considerado uma criança");
+}
+
+else if (Idade >= 13 && Idade <= 17)
+{
+    Console.WriteLine("Você é considerado um adolecente");
+}
+
+else if (Idade >= 18 && Idade <= 59)
+{
+    Console.WriteLine("Você é considerado um adulto");
+}
+
+else
+{
+    Console.WriteLine("Você é considerado um idoso");
+}
+
+#endregion
