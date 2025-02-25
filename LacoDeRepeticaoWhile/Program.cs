46869aa7ef5144402e8b8d9ed3a3de24fdae7a21
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-25 15:11:42 -0300

    Verificar se usuário é +18 para prosseguir com cadastro

---
+++ LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,38 @@
+﻿#region Verificar se usuario é +18 para prosseguir com cadastro
+
+
+int Idade;
+string Nome;
+string RG;
+string CPF;
+
+while(true)
+{
+    Console.WriteLine("Digite sua idade: ");
+    Idade = int.Parse(Console.ReadLine());
+
+    if (Idade >= 18)
+    {
+        Console.WriteLine("Idade válida ");
+        break;
+    }
+    else { 
+
+     Console.WriteLine("Você não tem idade suficiente para prosseguir com o cadastro");
+    }
+
+ }
+
+Console.WriteLine("Digite seu nome: ");
+Nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu RG: ");
+RG = Console.ReadLine();
+
+Console.WriteLine("Digite seu CPF: ");
+CPF = Console.ReadLine();
+
+Console.WriteLine("Cadastro realizado com sucesso!");
+
+
+#endregion
