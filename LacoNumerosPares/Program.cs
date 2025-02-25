2f37c01e85ddee3f2310e202c03ba4459a7ac0a7
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-25 16:03:08 -0300

    Exibir números pares de 1 a 50

---
+++ LacoNumerosPares/Program.cs
@@ -0,0 +1,20 @@
+﻿#region Verificação de numeros pares
+
+{
+
+ }
+
+     int numero = 2; // Começamos com o primeiro número par
+
+         Console.WriteLine("Números pares entre 1 e 50: "); 
+
+
+   while (numero <= 50)
+
+ {
+         Console.WriteLine(numero);
+         numero += 2; // Adicionamos 2 em 2 para obter o próximo número par
+}
+   
+
+#endregion
