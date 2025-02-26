651b94bbfe0e5043a7d3808b151a11aa77b009fc
Aline Fernandes <alinefernandeslima@icloud.com>
2025-02-26 17:55:01 -0300

    Calcula media de estoque minimo e máximo.  Usado variavel ushort para não aceitar numeros negativos.

---
+++ CalculaMediaEstoque/Program.cs
@@ -0,0 +1,31 @@
+﻿#region Programa calcula o estoque médio de uma peça
+
+ushort EstoqueMinimo;
+ushort EstoqueMaximo;
+ushort EstoqueMedio;
+
+while (true) // Loop infinito até que valores válidos sejam inseridos
+{
+    Console.WriteLine("Digite o estoque mínimo da peça: ");
+    if (ushort.TryParse(Console.ReadLine(), out EstoqueMinimo))
+    {
+        break; // Sai do loop se a entrada for válida
+    }
+    Console.WriteLine("Valor inválido, tente novamente.");
+}
+
+
+while (true) // Loop infinito até que valores válidos sejam inseridos
+{
+    Console.WriteLine("Digite o estoque máximo da peça: ");
+    if (ushort.TryParse(Console.ReadLine(), out EstoqueMaximo))
+    {
+        break; // Sai do loop se a entrada for válida
+    }
+        Console.WriteLine("Valor inválido, tente novamente.");
+    }
+    
+EstoqueMedio = (ushort)((EstoqueMinimo + EstoqueMaximo) / 2);
+Console.WriteLine("O estoque médio da peça é: " + EstoqueMedio);
+
+#endregion
